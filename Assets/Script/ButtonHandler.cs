5e6d6c496529ea1406067a00092ac43266c50c1c
Emilio Valdivia <evaldiviaci@unsa.edu.pe>
2021-12-15 18:38:26 -0500

    1st done

--- Assets/Script/ButtonHandler.cs
+++ Assets/Script/ButtonHandler.cs
@@ -6,7 +6,8 @@ public class ButtonHandler : MonoBehaviour
 {
     public GameObject depar;
     public GameObject maincanvas;
-    public GameObject imbbg1,imbg2;
+    public GameObject imbbg1,imbg2,bg3, bg4,bg5,bg6,bg7,bg8,bg9,bg10
+        ,bg11,bg12,bg13,bg14,bg15,bg16,bg17,bg18,bg19,bg20,bg21,bg22,bg23,bg24,bg25;
 
     // Start is called before the first frame update
     void Start()
@@ -27,12 +28,38 @@ public class ButtonHandler : MonoBehaviour
     }
     public void OcultarDepartamento()
     {
+        maincanvas.SetActive(true);
+
+
         depar.SetActive(false);
         imbbg1.SetActive(false);
         imbg2.SetActive(false);
+        bg3.SetActive(false);
+        bg4.SetActive(false);
+        bg5.SetActive(false);
+        bg6.SetActive(false);
+        bg7.SetActive(false);
+        bg8.SetActive(false);
+        bg9.SetActive(false);
+        bg10.SetActive(false);
+        bg11.SetActive(false);
+        bg12.SetActive(false);
+        bg13.SetActive(false);
+        bg14.SetActive(false);
+        bg15.SetActive(false);
+        bg16.SetActive(false);
+        bg17.SetActive(false);
+        bg18.SetActive(false);
+        bg19.SetActive(false);
+        bg20.SetActive(false);
+        bg21.SetActive(false);
+        bg22.SetActive(false);
+        bg23.SetActive(false);
+        bg24.SetActive(false);
+        bg25.SetActive(false);
 
-        maincanvas.SetActive(true);
+
     }
-    
+
 
 }
--- Assets/Script/UIAppear.cs
+++ Assets/Script/UIAppear.cs
@@ -5,7 +5,15 @@ using UnityEngine.UI;
 
 public class UIAppear : MonoBehaviour
 {
-    [SerializeField] private GameObject buttnCajamarca,bttnLoreto;
+    [SerializeField] private GameObject
+        Amazonas, Ancash, Apurimac, 
+        Arequipa, Ayacucho, Cajamarca, 
+        Callao, Cusco, Huancavelica, 
+        Huanuco, Ica, Junin, LaLibertad, 
+        Lambayeque, Lima, Loreto, 
+        MadredeDios, Moquegua, Pasco, Piura, 
+        Puno, SanMartin, Tacna, Tumbes,
+        Ucayali;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,24 +27,209 @@ public class UIAppear : MonoBehaviour
     }
     void OnTriggerEnter2D(Collider2D collision)
     {
+        
+        if (collision.CompareTag("Amazonas"))
+        {
+            Amazonas.SetActive(true);
+        }
+        if (collision.CompareTag("Ancash"))
+        {
+            Ancash.SetActive(true);
+        }
+        if (collision.CompareTag("Apurimac"))
+        {
+            Apurimac.SetActive(true);
+        }
+        if (collision.CompareTag("Arequipa"))
+        {
+            Arequipa.SetActive(true);
+        }
+        if (collision.CompareTag("Ayacucho"))
+        {
+            Ayacucho.SetActive(true);
+        }
         if (collision.CompareTag("Cajamarca"))
         {
-            buttnCajamarca.SetActive(true);
+            Cajamarca.SetActive(true);
+        }
+        if (collision.CompareTag("Cusco"))
+        {
+            Cusco.SetActive(true);
+        }
+        if (collision.CompareTag("Huancavelica"))
+        {
+            Huancavelica.SetActive(true);
+        }
+        if (collision.CompareTag("Huanuco"))
+        {
+            Huanuco.SetActive(true);
+        }
+        if (collision.CompareTag("Ica"))
+        {
+            Ica.SetActive(true);
+        }
+        if (collision.CompareTag("Junin"))
+        {
+            Junin.SetActive(true);
+        }
+        if (collision.CompareTag("LaLibertad"))
+        {
+            LaLibertad.SetActive(true);
+        }
+        if (collision.CompareTag("Lambayeque"))
+        {
+            Lambayeque.SetActive(true);
+        }
+        if (collision.CompareTag("Lima"))
+        {
+            Lima.SetActive(true);
         }
         if (collision.CompareTag("Loreto"))
         {
-            bttnLoreto.SetActive(true);
+            Loreto.SetActive(true);
+        }
+        if (collision.CompareTag("MadredeDios"))
+        {
+            MadredeDios.SetActive(true);
+        }
+        if (collision.CompareTag("Moquegua"))
+        {
+            Moquegua.SetActive(true);
+        }
+        if (collision.CompareTag("Pasco"))
+        {
+            Pasco.SetActive(true);
+        }
+        if (collision.CompareTag("Piura"))
+        {
+            Piura.SetActive(true);
+        }
+        if (collision.CompareTag("Puno"))
+        {
+            Puno.SetActive(true);
+        }
+        if (collision.CompareTag("SanMartin"))
+        {
+            SanMartin.SetActive(true);
+        }
+        if (collision.CompareTag("Tacna"))
+        {
+            Tacna.SetActive(true);
+        }
+        if (collision.CompareTag("Tumbes"))
+        {
+            Tumbes.SetActive(true);
+        }
+        if (collision.CompareTag("Ucayali"))
+        {
+            Ucayali.SetActive(true);
+        }
+        if (collision.CompareTag("Callao"))
+        {
+            Callao.SetActive(true);
         }
     }
     void OnTriggerExit2D(Collider2D collision)
     {
+        if (collision.CompareTag("Amazonas"))
+        {
+            Amazonas.SetActive(false);
+        }
+        if (collision.CompareTag("Ancash"))
+        {
+            Ancash.SetActive(false);
+        }
+        if (collision.CompareTag("Apurimac"))
+        {
+            Apurimac.SetActive(false);
+        }
+        if (collision.CompareTag("Arequipa"))
+        {
+            Arequipa.SetActive(false);
+        }
+        if (collision.CompareTag("Ayacucho"))
+        {
+            Ayacucho.SetActive(false);
+        }
         if (collision.CompareTag("Cajamarca"))
         {
-            buttnCajamarca.SetActive(false);
+            Cajamarca.SetActive(false);
+        }
+        if (collision.CompareTag("Cusco"))
+        {
+            Cusco.SetActive(false);
+        }
+        if (collision.CompareTag("Huancavelica"))
+        {
+            Huancavelica.SetActive(false);
+        }
+        if (collision.CompareTag("Huanuco"))
+        {
+            Huanuco.SetActive(false);
+        }
+        if (collision.CompareTag("Ica"))
+        {
+            Ica.SetActive(false);
+        }
+        if (collision.CompareTag("Junin"))
+        {
+            Junin.SetActive(false);
+        }
+        if (collision.CompareTag("LaLibertad"))
+        {
+            LaLibertad.SetActive(false);
+        }
+        if (collision.CompareTag("Lambayeque"))
+        {
+            Lambayeque.SetActive(false);
+        }
+        if (collision.CompareTag("Lima"))
+        {
+            Lima.SetActive(false);
         }
         if (collision.CompareTag("Loreto"))
         {
-            bttnLoreto.SetActive(false);
+            Loreto.SetActive(false);
+        }
+        if (collision.CompareTag("MadredeDios"))
+        {
+            MadredeDios.SetActive(false);
+        }
+        if (collision.CompareTag("Moquegua"))
+        {
+            Moquegua.SetActive(false);
+        }
+        if (collision.CompareTag("Pasco"))
+        {
+            Pasco.SetActive(false);
+        }
+        if (collision.CompareTag("Piura"))
+        {
+            Piura.SetActive(false);
+        }
+        if (collision.CompareTag("Puno"))
+        {
+            Puno.SetActive(false);
+        }
+        if (collision.CompareTag("SanMartin"))
+        {
+            SanMartin.SetActive(false);
+        }
+        if (collision.CompareTag("Tacna"))
+        {
+            Tacna.SetActive(false);
+        }
+        if (collision.CompareTag("Tumbes"))
+        {
+            Tumbes.SetActive(false);
+        }
+        if (collision.CompareTag("Ucayali"))
+        {
+            Ucayali.SetActive(false);
+        }
+        if (collision.CompareTag("Callao"))
+        {
+            Callao.SetActive(false);
         }
     }
 }
